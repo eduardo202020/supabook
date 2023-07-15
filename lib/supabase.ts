ddd79b8fc04c1e3023224b95e341f7d075251f94
Eduardo <jguevaral@uni.pe>
2023-07-15 14:34:47 -0500

    FIX env

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -5,8 +5,11 @@ import Constants from "expo-constants";
 import { Database } from "../db_types";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const supabaseUrl = Constants?.expoConfig?.extra?.supabaseUrl;
-const supabaseAnonKey = Constants?.expoConfig?.extra?.supabaseAnonKey;
+const supabaseUrl =
+  Constants?.expoConfig?.extra?.supabaseUrl || process.env.SUPABASE_URL;
+const supabaseAnonKey =
+  Constants?.expoConfig?.extra?.supabaseAnonKey ||
+  process.env.SUPABASE_ANON_KEY;
 
 export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey, {
   auth: {
